f3c0314d6f3441c1ef303fe8cf3ac39120e9c50e
Nguyen Nguyen <ntnguyenit@gmail.com>
2020-09-10 21:53:09 +0700

    Update main.go

--- worker-pool/main.go
+++ worker-pool/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func worker(id int, pool <-chan int, results chan<- int) {
+	//The loop for job := range pool receives values from the channel repeatedly until it is closed.
 	for job := range pool {
 		fmt.Println("Worker", id, " started job ", job)
 		time.Sleep(time.Microsecond * 2)
@@ -24,12 +25,15 @@ func main() {
 		go worker(i, pool, results)
 	}
 
-	//Create 10 jobs
+	//Create jobs and push them to pool
 	for j := 1; j <= numJobs; j++ {
 		pool <- j
 	}
+	//close a channel to indicate that no more values will be sent
+	//Closing is only necessary when the receiver must be told there are no more values coming, such as to terminate a range loop.
 	close(pool)
 
+	//Read the result and block main routine until no more values from the results channel 
 	for a := 1; a <= numJobs; a++ {
 		fmt.Println("Result", <-results)
 	}
